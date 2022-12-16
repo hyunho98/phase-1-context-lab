99367b4be6bc4c4d1f2545b26ee34d24a418904e
Alex Lee <hyunho98@gmail.com>
2022-12-16 09:38:49 -0500

    commit tree edecc559ad13d70d56ecb1aba7f213bf94f68079

--- index.js
+++ index.js
@@ -1,4 +1,74 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+    const eRecord = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+
+    return eRecord;
+}
+
+function createEmployeeRecords(array) {
+    const eRecords = array.map(employee => createEmployeeRecord(employee));
+    
+    return eRecords;
+}
+
+function createTimeInEvent(date) {
+    const splitDate = date.split(' ');
+    const event = {
+        type: "TimeIn",
+        hour: parseInt(splitDate[1]),
+        date: splitDate[0]
+    }
+    
+    this.timeInEvents.push(event);
+    return this;
+}
+
+function createTimeOutEvent(date) {
+    const splitDate = date.split(' ');
+    const event = {
+        type: "TimeOut",
+        hour: parseInt(splitDate[1]),
+        date: splitDate[0]
+    }
+
+    this.timeOutEvents.push(event);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeInEvents = this.timeInEvents;
+    const timeOutEvents = this.timeOutEvents;
+
+    for (let i = 0; i < timeInEvents.length; i++) {
+        if (timeInEvents[i].date === date)
+            return(timeOutEvents[i].hour - timeInEvents[i].hour) / 100;
+    }
+    
+    return 0;
+}
+
+function wagesEarnedOnDate(date) {
+    const wages = hoursWorkedOnDate.call(this, date) * parseInt(this.payPerHour);
+    return wages;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    const employee = srcArray.find(employee => employee.firstName === firstName);
+    return employee;
+}
+
+function calculatePayroll(srcArray) {
+    const eWages = srcArray.map(employee => allWagesFor.call(employee));
+    const payroll = eWages.reduce((total, wage) => total + wage);
+    return payroll;
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +90,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
